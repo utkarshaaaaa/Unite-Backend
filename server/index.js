6bb6d524122185daa2de32659891b9f552a60a04
utkarshaaaaa <vyasutkarsh11@gmail.com>
2026-03-10 12:57:10 +0530

    commit

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ const app = express();
 const server = http.createServer(app);
 app.use(
   cors({
-    origin: "https://unite0.onrender.com", 
+    origin: "https://unite0.onrender.com", //ocalhost:5173
     credentials: true,               
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type"],
